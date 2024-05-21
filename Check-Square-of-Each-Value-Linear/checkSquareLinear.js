34a8bd0bb85dffc6476747474cd9783fccca6ee6
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-21 18:40:55 +0500

    Check Square of the each Value in the given Array

--- Check-Square-of-Each-Value-Linear/checkSquareLinear.js
+++ Check-Square-of-Each-Value-Linear/checkSquareLinear.js
@@ -1,1 +1,30 @@
+// LINEAR TIME COMPLEXITY
+// CHECK SQUARE OF THE EACH VALUE IN THE GIVEN ARRAY
 
+const checkSquare = (arr1, arr2) => {
+  let obj1 = {};
+  let obj2 = {};
+
+  for (let item1 of arr1) {
+    obj1[item1] = (obj1[item1] || 0) + 1;
+  }
+
+  for (let item2 of arr2) {
+    obj2[item2] = (obj2[item2] || 0) + 1;
+  }
+
+  for (key in obj1) {
+    if (!obj2[key * key]) {
+      return false;
+    }
+
+    if (obj1[key] !== obj2[key * key]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const result = checkSquare([4, 3, 2, 1], [16, 9, 4, 1]);
+console.log(result);
