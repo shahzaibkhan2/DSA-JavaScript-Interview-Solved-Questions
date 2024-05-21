818771977a8d7f544b6403d1af676c561f44fcaa
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-21 18:53:27 +0500

    Find Odd Number using Helper Recursion Method

--- Helper-Recursions/Find-Odd-Number/findOddNum.js
+++ Helper-Recursions/Find-Odd-Number/findOddNum.js
@@ -1,1 +1,19 @@
+// FIND ODD NUMBERS USING RECURSION METHOD
 
+const findOddNum = (arr) => {
+  let result = [];
+  function recursionHelper(helperArray) {
+    if (helperArray.length === 0) {
+      return;
+    } else if (helperArray[0] % 2 !== 0) {
+      result.push(helperArray[0]);
+    }
+    recursionHelper(helperArray.slice(1));
+  }
+
+  recursionHelper(arr);
+  return result;
+};
+
+const res = findOddNum([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(res);
