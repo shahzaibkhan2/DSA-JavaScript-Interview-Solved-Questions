793401ecd335ff9af6e2c309d40c9b71a855e191
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-21 17:33:12 +0500

    Find Anagram using Quadratic Time Complexity

---
+++ Find-Anagram/findAnagramQuadComplex.js
@@ -0,0 +1,21 @@
+const findAnagram = (str1, str2) => {
+  let counter = {};
+
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  for (let key of str1) {
+    counter[key] = (counter[key] || 0) + 1;
+  }
+
+  for (let key of str2) {
+    if (!counter[key]) {
+      return false;
+    }
+
+    counter[key] -= 1;
+  }
+
+  return true;
+};
