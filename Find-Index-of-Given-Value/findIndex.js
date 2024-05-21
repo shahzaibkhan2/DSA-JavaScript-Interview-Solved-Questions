8c8f15e3b2c58ff48bfc5d8025eb8f5265895825
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-21 18:19:48 +0500

    Find Index of the given value

--- Find-Index-of-Given-Value/findIndex.js
+++ Find-Index-of-Given-Value/findIndex.js
@@ -1,1 +1,20 @@
+const findIndex = (arr, num) => {
+  let minIndex = 0;
+  let maxIndex = arr.length - 1;
 
+  while (minIndex <= maxIndex) {
+    let midIndex = Math.floor((minIndex + maxIndex) / 2);
+    if (arr[midIndex] > num) {
+      maxIndex = midIndex - 1;
+    } else if (arr[midIndex] < num) {
+      minIndex = midIndex + 1;
+    } else {
+      return midIndex;
+    }
+  }
+
+  return -1;
+};
+
+const result = findIndex([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1);
+console.log(result);
