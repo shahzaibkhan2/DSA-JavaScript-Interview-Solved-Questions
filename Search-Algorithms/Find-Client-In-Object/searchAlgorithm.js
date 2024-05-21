42b2a901923ee4673a31de7890ca8f9dde11a2bf
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-21 19:01:25 +0500

    Search Algorithm to find any user in object

--- Search-Algorithms/Find-Client-In-Object/searchAlgorithm.js
+++ Search-Algorithms/Find-Client-In-Object/searchAlgorithm.js
@@ -1,1 +1,18 @@
+// SEARCH ALGORITHM TO FIND SPECIFIC USER / CLIENT IN THE OBJECTS AND ARRAYS
 
+const clients = [
+  { name: "shahzaib", email: "shahzaib@gmail.com" },
+  { name: "xyz", email: "xyz@gmail.com" },
+];
+
+const findClient = (arr, client) => {
+  for (let item of arr) {
+    if (item.name === client) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const res = findClient(clients, "shahza");
+console.log(res);
