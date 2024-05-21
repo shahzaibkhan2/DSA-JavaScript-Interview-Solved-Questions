ed72bfa93de501f86dda57a68aeb10204a841021
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-21 17:41:49 +0500

    Find Unique Number

--- Find-Unique-Number/findUniqueNum.js
+++ Find-Unique-Number/findUniqueNum.js
@@ -1,1 +1,17 @@
+const findUniqueNumber = (array) => {
+  if (array.length > 0) {
+    let i = 0;
+    for (let j = 1; j < array.length; j++) {
+      if (array[i] !== array[j]) {
+        i++;
+        array[i] = array[j];
+      }
+    }
+    return i + 1;
+  } else {
+    console.log("Array is empty");
+  }
+};
 
+const result = findUniqueNumber([11, 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 8, 9, 9, 9]);
+console.log(result);
