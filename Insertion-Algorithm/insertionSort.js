47f514d1f61f1c97e1577cbb524a596a2ed70db7
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-25 13:13:40 +0500

    Insertion Sort Algorithm

--- Insertion-Algorithm/insertionSort.js
+++ Insertion-Algorithm/insertionSort.js
@@ -1,1 +1,19 @@
+// INSERTION
 
+const insertionSort = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let current = arr[i];
+    let leftVal = i - 1;
+
+    while (leftVal >= 0 && arr[leftVal] > current) {
+      arr[leftVal + 1] = arr[leftVal];
+      leftVal--;
+    }
+    arr[leftVal + 1] = current;
+  }
+
+  return arr;
+};
+
+const result = insertionSort([8, 2, 3, 1]);
+console.log(result);
