87126a5b03719e4fa1182393837437197dd91682
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-25 11:03:58 +0500

    Selection Sort Algorithm

--- Selection-Sort/selectionSort.js
+++ Selection-Sort/selectionSort.js
@@ -1,1 +1,22 @@
+// SELECTION SORT ALGORITHM
 
+const selectionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      let tempVar = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tempVar;
+    }
+  }
+
+  return arr;
+};
+
+const result = selectionSort([11, 21, 12, 2, 1, 4, 3, 6, 5]);
+console.log(result);
