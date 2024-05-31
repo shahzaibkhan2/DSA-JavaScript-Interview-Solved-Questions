004996ee3d4d5bbad97f8ade7d16bad4c6d9fdcf
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-31 17:51:44 +0500

    Find the Max Occurring Letter / Character

--- Max-Occurring-Character/maxOccurringLetter.js
+++ Max-Occurring-Character/maxOccurringLetter.js
@@ -1,1 +1,24 @@
+const maxOccurringLetter = (str) => {
+  let tempObj = {};
+  str
+    .toLowerCase()
+    .split("")
+    .forEach((element) => {
+      tempObj[element] = tempObj[element] ? tempObj[element] + 1 : 1;
+    });
 
+  let max = 1;
+  let letter = str[0];
+
+  for (let key in tempObj) {
+    if (tempObj[key] > max) {
+      max = tempObj[key];
+      letter = key;
+    }
+  }
+
+  return letter;
+};
+
+const result = maxOccurringLetter("React is Awesome");
+console.log(result);
