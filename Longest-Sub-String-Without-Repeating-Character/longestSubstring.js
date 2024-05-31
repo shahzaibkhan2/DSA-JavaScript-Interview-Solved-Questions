67c0a1dc46188cf3de5e22ee9b355defe96d8655
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-31 19:52:02 +0500

    Find Longest SubString Without Repeating Character

--- Longest-Sub-String-Without-Repeating-Character/longestSubstring.js
+++ Longest-Sub-String-Without-Repeating-Character/longestSubstring.js
@@ -1,1 +1,27 @@
+// Find the longest Sub-string without repeating characters
 
+// TECHNIQUE: Window Slider Technique
+
+const longestSubstring = (str) => {
+  let startPointer = 0;
+  let endPointer = 0;
+
+  let maxLength = 0;
+  const uniqueChars = new Set();
+
+  while (endPointer < str.length) {
+    if (!uniqueChars.has(str[endPointer])) {
+      uniqueChars.add(str[endPointer]);
+      endPointer++;
+      maxLength = Math.max(maxLength, uniqueChars.size);
+    } else {
+      uniqueChars.delete(str[startPointer]);
+      startPointer++;
+    }
+  }
+
+  return maxLength;
+};
+
+const result = longestSubstring("abcabcbb");
+console.log(result);
