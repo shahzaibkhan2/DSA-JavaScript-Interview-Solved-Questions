7bf8e10fc8db5fabb41f26e9656bfc1c5140a6f5
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-05-31 19:05:47 +0500

    Check if the given string is Palindrome or not

--- Check-Palindrome/checkPalindrome.js
+++ Check-Palindrome/checkPalindrome.js
@@ -1,1 +1,33 @@
+// WITH BUILT IN JAVASCRIPT FUNCTIONS AND METHODS
 
+const checkPalindrome = (str) => {
+  let newStr = str.split("").reverse().join("");
+
+  return newStr.toLowerCase() === str.toLowerCase();
+};
+
+const result = checkPalindrome("deed");
+console.log(result);
+
+// ------------------------------ 2nd Question ---------------------------------------
+
+// WITHOUT BUILT IN JAVASCRIPT FUNCTIONS AND METHODS AND ACCORDING TO THE DSA
+
+const modifiedCheckPalindrome = (str) => {
+  let newStr = str.toLowerCase();
+
+  let leftPointer = 0;
+  let rightPointer = newStr.length - 1;
+
+  while (leftPointer < rightPointer) {
+    if (newStr[leftPointer] !== newStr[rightPointer]) return false;
+
+    leftPointer++;
+    rightPointer--;
+  }
+
+  return true;
+};
+
+result2 = modifiedCheckPalindrome("deed");
+console.log(result2);
