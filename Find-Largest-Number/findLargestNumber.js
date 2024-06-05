7bdb26d70366028f0e354062716a492ac150a3e1
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-06-05 13:25:07 +0500

    Find the largest number in the given array

--- Find-Largest-Number/findLargestNumber.js
+++ Find-Largest-Number/findLargestNumber.js
@@ -1,1 +1,18 @@
+const findLargestNum = (array) => {
+  let largest = array[0];
 
+  if (array.length === 1) {
+    console.log("Largest Number is : ", largest);
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (largest < array[i]) {
+        largest = array[i];
+      }
+    }
+  }
+
+  return `Largest Number is : ${largest}`;
+};
+
+const result = findLargestNum([2343434, 33, 22, 33, 342, 123, 2235, 10000, 1]);
+console.log(result);
