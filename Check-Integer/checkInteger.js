a1563477abc1298bb383598e7eeed63c64049f64
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-06-10 10:22:09 +0500

    Check Integer or not

--- Check-Integer/checkInteger.js
+++ Check-Integer/checkInteger.js
@@ -1,1 +1,23 @@
+// DSA approach
 
+const checkInteger = (param) => {
+  if (param % 1 === 0) {
+    return `${param} is integer.`;
+  } else {
+    return `${param} is not integer.`;
+  }
+};
+
+console.log(checkInteger(3.43));
+
+// Built-in JavaScript methods approach
+
+const checkIntegerBuiltIn = (param) => {
+  if (Number.isInteger(param)) {
+    return `${param} is integer.`;
+  } else {
+    return `${param} is not integer.`;
+  }
+};
+
+console.log(checkIntegerBuiltIn(2.2));
