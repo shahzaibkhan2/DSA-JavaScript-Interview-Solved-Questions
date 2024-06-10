4bafe5ed3ade11249c97a2e2a66c356550b45424
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-06-10 10:23:25 +0500

    Reverse the given number

--- Reverse-Number/reverseNumber.js
+++ Reverse-Number/reverseNumber.js
@@ -1,1 +1,22 @@
+// DSA Approach
 
+const reverseNumber = (num) => {
+  let reversed = 0;
+  while (num > 0) {
+    let lastValue = num % 10;
+    reversed = reversed * 10 + lastValue;
+    num = Math.floor(num / 10);
+  }
+
+  return reversed;
+};
+
+console.log(reverseNumber(123456789));
+
+// Built-in JavaScript methods approach
+
+const reverseNumberBuiltIn = (num) => {
+  return Number(num.toString().split("").reverse().join(""));
+};
+
+console.log(reverseNumberBuiltIn(123456789));
