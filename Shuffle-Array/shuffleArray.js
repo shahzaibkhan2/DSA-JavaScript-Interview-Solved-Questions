cc614c72e56e083481c4c5f66c2182698e85c6d3
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-06-10 10:20:47 +0500

    Shuffle Array using DSA approach

--- Shuffle-Array/shuffleArray.js
+++ Shuffle-Array/shuffleArray.js
@@ -1,1 +1,16 @@
+const shuffleArray = (arr) => {
+  let totalValuesLength = arr.length;
 
+  while (totalValuesLength > 0) {
+    totalValuesLength--;
+
+    let newIndex = Math.floor(Math.random() * totalValuesLength);
+    let tempValue = arr[totalValuesLength];
+    arr[totalValuesLength] = arr[newIndex];
+    arr[newIndex] = tempValue;
+  }
+
+  return arr;
+};
+
+console.log(shuffleArray([1, 4, 5, 7, 8, 9, 0, 12, 33]));
