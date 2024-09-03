596136ae4e4efce3837b312eaae4e635e8313491
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-09-03 17:16:19 +0500

    find Duplicates

--- find-duplicates-in-array/findDuplicates.js
+++ find-duplicates-in-array/findDuplicates.js
@@ -1,1 +1,22 @@
-find Duplicates
+// Find Duplicates
+// Linear Time Complexity
+
+const findDuplicates = (arr) => {
+  let seen = {};
+  let duplicates = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (seen[arr[i]]) {
+      if (seen[arr[i]] === 1) {
+        duplicates.push(arr[i]);
+      }
+      seen[arr[i]]++;
+    } else {
+      seen[arr[i]] = 1;
+    }
+  }
+
+  return duplicates;
+};
+
+console.log(findDuplicates([1, 1, 2, 3, 4, 5, 2, 3, 7, 8, 9]));
