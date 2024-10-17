027ea66a9cb5582962da314e1daed9648aea2284
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-10-17 10:41:52 +0500

    Find Longest Word

--- Common_String_Questions/findLongestWord.js
+++ Common_String_Questions/findLongestWord.js
@@ -1,1 +1,25 @@
+// Case 1
+const findLongestString = (str) => {
+  if (str.trim().length < 1) return "Please provide a string.";
+  let myArr = str.split(" ");
+  myArr.sort((a, b) => a.length - b.length);
+  return myArr.at(-1);
+};
 
+// Case 2
+
+const findLongestStringInArray = (str) => {
+  if (str.trim().length < 1) return "Please provide a string.";
+  let myArr = str.split(" ");
+  return myArr.reduce((acum, res) => (acum.length > res.length ? acum : res));
+};
+
+const example = findLongestString(
+  "Hello world ! This is the javascript question"
+);
+console.log(example);
+
+const example2 = findLongestStringInArray(
+  "Hello world ! This is the javascript question"
+);
+console.log(example2);
