c1988f5131f15b3b50648b2023c8c0ff9a505e2a
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-11-02 18:24:27 +0500

    Is input the power of two

--- Other-Interview-Questions/isPowerOfTwo.js
+++ Other-Interview-Questions/isPowerOfTwo.js
@@ -1,1 +1,13 @@
-All the interview questions
+const isPowerOfTwo = (num) => {
+  let flag = false;
+  for (let i = 1; i < num; i++) {
+    if (2 ** i === 8) flag = true;
+  }
+
+  return flag;
+};
+
+const example1 = isPowerOfTwo(4);
+const example2 = isPowerOfTwo(3);
+console.log(example1);
+console.log(example2);
