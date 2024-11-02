0078797b5212436eb6932f64c97a580767531325
Shahzaib Khan <122774230+shahzaibkhan2@users.noreply.github.com>
2024-11-02 17:47:39 +0500

    Are both the arrays equal
    Are both the arrays equal.

--- Array-Interview-Questions/areArraysEqual.js
+++ Array-Interview-Questions/areArraysEqual.js
@@ -1,1 +1,8 @@
-Common Array interview questions
+const areArraysEqual = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  return arr1.every((val, i) => val === arr2[i]);
+};
+
+const example = areArraysEqual([1, 2, 3], [1, 2, 4]);
+const example2 = areArraysEqual([1, 2, 3], [1, 2, 3]);
+console.log(`Example One is ${example} & Example Two is ${example2}`);
